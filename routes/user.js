53b6f641c39096e0dc34bfd68a885025c63b0fc7
JyotishnaBaishya <73800133+JyotishnaBaishya@users.noreply.github.com>
2020-11-03 16:49:29 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "WMP"
+});
+router.get('/login', function(req, res) {
+	res.render('login');
+});
+router.get('/register', function (req, res) {
+  res.render('register');
+});
+router.post('/create', function(req, res) {
+
+
+  	const userDetails=req.body;
+      var name= req.body.fname;
+      var address= req.body.address;
+      var contact1= (req.body.contact1);
+      var contact2= (req.body.contact2);
+      var email=req.body.email;
+      var descp=req.body.descp;
+      var Beds=(req.body.Beds);
+      var Amb=(req.body.Amb);
+      var pass=(req.body.password);
+      con.connect(function(err) {
+  		if (err) throw err;
+  		console.log("Connected!");
+  		var sql = "INSERT INTO hospital (name, address, contact1, contact2, email, descp, Beds, Amb, Password) VALUES ?";
+    var values = [
+      [name, address, contact1, contact2, email, descp, Beds, Amb, pass]
+    ];
+    con.query(sql, [values], function (err, data) {
+      if (err) throw err;
+      console.log("1 record inserted.");
+    });
+    var sql2="Select LAST_INSERT_ID() As 'hid'";
+    con.query(sql2, function(err, data, fields){
+      req.session.uname=data[0].hid;
+      console.log(data);
+      res.redirect('/welcome');
+    });
+  });
+});
+router.get('/welcome', function (req, res) {
+  res.render('welcome', { Uname: req.session.uname});
+});
+module.exports = router;
